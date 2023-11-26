aa16eea18a066ce41b450c5df2b2a224b3b9138e
Dragon of Shuu <68718280+DragonOfShuu@users.noreply.github.com>
2023-11-25 23:59:16 -0700

    Help Examples and Commands

--- ducklingscript/cli/help.py
+++ ducklingscript/cli/help.py
@@ -7,43 +7,94 @@ from rich import print
 from rich.markdown import Markdown
 from rich.console import Console
 from rich.style import Style
+from rich.markup import escape
+
+from ..compiler.commands.bases.simple_command import SimpleCommand
+from ..compiler.commands.bases.base_command import BaseCommand
 
 from ducklingscript.compiler.commands.bases.doc_command import ArgReqType, ComDoc
 
+class HelpConsole(Console):
+    def print_md(self, markup: str, color: str|None=None):
+        self.print(Markdown(markup), style=Style(color=color))
+
+    def print_labeled(self, label: str, text: str, color: str|None = None, extraline: bool = True, markdown: bool = True):
+        new_line = '' if extraline else None
+        if markdown:
+            self.print_color(
+                f"[bold]{label}: ",Markdown(text.strip()),new_line,color=color
+            )
+        else:
+            self.print_color(
+                f"[bold]{label}:\n",text.strip(),'\n'if extraline else '',color=color
+            )
+
+    def print_color(self, *text: object, color: str|None = None, markup: bool = True):
+        self.print(
+            *text,style=Style(color=color), markup=markup 
+        )
+
+
 
 def help(
-    command_name: Annotated[str, typer.Argument(help="The command to give a description on")]
+    command_name: Annotated[str, typer.Argument(help="The command to give a description on")],
+    examples: Annotated[bool, typer.Option(help="Give examples of the command")] = False
 ):
-    console = Console()
+    console = HelpConsole()
 
     com_doc = Compiler.get_docs(command_name)
     command = Compiler.get_command(command_name)
 
-    if com_doc is None:
+    if com_doc is None or command is None:
         print(f"[bold red]The command {command_name} does not exist. ⛔[/bold red]")
         return
+    
+    if not examples:
+        print_command(console, com_doc, command)
+    else:
+        print_examples(console, com_doc)
+
+
+def print_command(console: HelpConsole, com_doc: ComDoc, command: type[BaseCommand]):
+    console.print_color(f":wrench: This command is a {command.__base__.__name__}", color="deep_sky_blue3")
 
     if com_doc.flipper_only:
-        print(
-            f"[royal_blue1]:exclamation: This command is flipper only! :exclamation:[/royal_blue1]"
+        console.print_color(
+            ":exclamation: This command is flipper only! :exclamation:", 
+            color="royal_blue1"
         )
 
-    print(f"[orchid]Names: {', '.join(com_doc.names)}[/orchid]")
+    console.print_color(
+        get_arg_req_text(com_doc.arg_req_type),
+        color="orange3"
+    )
 
-    print_desc(console, com_doc)
+    console.print_labeled("Names", ', '.join(com_doc.names), "orchid")
 
-    print(f"[orange3]{get_arg_req_text(com_doc.arg_req_type)}[/orange3]")
+    console.print_labeled(
+        "Description", 
+        com_doc.description if com_doc.description else '[no description provided]', 
+        'red'
+    )
 
     if com_doc.parameters is not None:
-        print(f"[gold3]Parameters: {com_doc.parameters}[/gold3]")
+        console.print_labeled(
+            "Parameters",
+            ", ".join(com_doc.parameters),
+            "gold3"
+        )
 
     if com_doc.arg_req_type != ArgReqType.NOTALLOWED:
         arg_type = com_doc.argument_type
-        print(
-            f"[yellow2]Argument type: {arg_type if isinstance(arg_type, str) else arg_type.__name__ }[/yellow2]"
+        console.print_labeled(
+            label="Argument type",
+            text=arg_type if isinstance(arg_type, str) else arg_type.__name__,
+            color="yellow2",
+            markdown=False
         )
-
-    print_examples(console, com_doc)
+    
+    if com_doc.examples:
+        console.print_color("[THIS COMMAND HAS EXAMPLES. Use --examples]", color="orange3")
 
 
 def get_arg_req_text(arg_req_type: ArgReqType) -> str:
@@ -55,38 +106,36 @@ def get_arg_req_text(arg_req_type: ArgReqType) -> str:
         case ArgReqType.NOTALLOWED:
             return "-> This command has no argument(s)"
 
-def print_examples(console: Console, com_doc: ComDoc):
-    if not com_doc.examples: return
+def print_examples(console: HelpConsole, com_doc: ComDoc):
+    if not com_doc.examples: 
+        console.print_color("[bold red]This command does not have examples. ⛔[/bold red]")
+        return
 
-    Console()
-
-    print(f"[aquamarine1]{Markdown('# Examples')}")
-    print("-->")
-    print_md(console, "")
-
-    for i in com_doc.examples:
+    for example_count,i in enumerate(com_doc.examples):
+        print(f"[bold yellow1]--> Example {example_count}")
         if i.file_structure:
-            print_md(console, "## File Structure")
+            console.print_labeled(
+                "File Structure",
+                "```"+i.file_structure+"```"
+            )
 
-        print_md(console, "## DucklingScript")
-        console.print(i.duckling)
+        console.print_labeled(
+            "DucklingScript",
+            "```"+i.duckling+"```",
+            color="blue"
+        )
 
         if i.compiled:
-            print_md(console, "## Compiled")
-            console.print(i.compiled)
+            console.print_labeled(
+                "Compiled",
+                "```"+i.compiled+"```"
+            )
         
         if i.std_out:
-            print_md(console, "## Console Output")
-            console.print(i.std_out)
+            console.print_labeled(
+                "Console Output",
+                "```"+i.std_out+"```",
+                color="aquamarine"
+            )
         
-        print("-->")
-    
-
-def print_desc(console: Console, com_doc: ComDoc):
-    console.print(
-        "[bold]Description: [/bold]", 
-        Markdown(com_doc.description if com_doc.description else '[no description provided]'), 
-        end='', style=Style(color="red"))
-
-def print_md(console: Console, markup: str):
-    console.print(Markdown(markup))
+    print("-->")
