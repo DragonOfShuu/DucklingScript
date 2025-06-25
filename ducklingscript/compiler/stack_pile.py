7fef491a1fee0be7bd6cfa97a3b2eceb4f3a7d11
DragonOfShuu <68718280+DragonOfShuu@users.noreply.github.com>
2025-06-24 23:56:22 -0600

    Remove Comment

--- ducklingscript/compiler/stack_pile.py
+++ ducklingscript/compiler/stack_pile.py
@@ -86,16 +86,6 @@ class StackPile:
         
         latest_stack = self.stack_pile[-1]
 
-        # WE NEED TO TURN THE ENVIRONMENT INTO SOMETHING THAT
-        # CAN EASILY BE EXTENDED BY A FUNCTION CALL.
-        # YOU'LL BE ABLE TO DO A PARALLEL ENVIRONMENT OR 
-        # A SEQUENTIAL ENVIRONMENT.
-
-        # WE COULD DO THIS BY HAVING YOU PASS A "PARALLEL" BOOL
-        # AND THE CURRENT ENVIRONMENT TO STACK. STACK COULD THEN
-        # USE THE EASY FUNCTION CALL, AND GIVE ITSELF INTO THE 
-        # FUNCTION CALL.
-
         new_stack = Stack(
             commands,
             self,
