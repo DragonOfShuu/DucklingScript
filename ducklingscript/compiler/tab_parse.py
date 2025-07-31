0d978c24e166f7cd7e8a58e63614f1f60b726847
Dragon of Shuu <68718280+DragonOfShuu@users.noreply.github.com>
2025-07-30 23:56:12 -0600

    Finished Connecting Functions & Classes, Tests Fail

--- ducklingscript/compiler/tab_parse.py
+++ ducklingscript/compiler/tab_parse.py
@@ -1,11 +1,16 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Generator, Iterable, Iterator, TypeVar, cast
 import re
 
 from .errors import InvalidTabError, UnclosedQuotationsError
 from .pre_line import PreLine, DimensionalPreLine
+
+
+@dataclass
+class DocumentParserNextOptions:
+    consume_new_tabs: bool = True
+    skip_empty_lines: bool = True
 
 
 def has_tab(i: str, tab_char: str | None, line: int) -> int | str:
@@ -26,7 +31,7 @@ def has_tab(i: str, tab_char: str | None, line: int) -> int | str:
     return 0
 
 
-def _get_free_tab(line: PreLine, gen: DocumentParser) -> tuple[list[PreLine], bool, PreLine|None]:
+def _get_free_tab(line: PreLine, gen: DocumentParser) -> PreLine|None:
     """
     Detects if this line is a free tab,
     and will forward the generator until 
@@ -39,57 +44,27 @@ def _get_free_tab(line: PreLine, gen: DocumentParser) -> tuple[list[PreLine], bo
 
     content = line.content.strip()
     if not content.startswith('"""') or not first_line: 
-        return [], first_line, line
+        return line
 
-    free_tab: list[PreLine] = []
-    next_line = next(gen, None)
+    # free_tab: list[PreLine] = []
+    options = DocumentParserNextOptions(False, False)
+    next_line = gen.next(options)
     while next_line and next_line.content.strip() != '\"\"\"':
-        free_tab.append(next_line)
-        next_line = next(gen, None)
+        # free_tab.append(next_line)
+        next_line = gen.add_line(next_line)
 
     if next_line is None:
         raise UnclosedQuotationsError(
             f"Unclosed quotations on line {line.number}. Please ensure that all quotations are closed."
         )
 
-    return free_tab, False, next(gen, None)
-
-
-# def _get_new_indents(line: PreLine, tab_char: str|None, first_line: bool, gen: Iterator[PreLine]) -> tuple[list[PreLine | list], bool, PreLine|None, str|None]:
-#     new_tabbed_list = []
-#     current_line: PreLine|None = line
-
-#     while current_line is not None and (tab := has_tab(current_line.content, tab_char, current_line.number)):
-#         if current_line.content.strip() == "":
-#             current_line = next(gen, None)
-#             continue
-        
-#         if first_line:
-#             raise InvalidTabError(f"Unexpected tab on line {current_line.number}")
-        
-#         if isinstance(tab, str):
-#             tab_char = tab
-        
-#         new_tab_char = cast(str, tab_char)
-        
-#         new_line = current_line.content.removeprefix(new_tab_char)
-#         new_tabbed_list.append(PreLine(new_line, current_line.number, current_line.file_index))
-#         current_line = next(gen, None)
-    
-#     return parse_document(new_tabbed_list, tab_char), first_line, current_line, tab_char
-
-
-@dataclass
-class DocumentParserNextOptions:
-    consume_new_tabs = False
-    skip_empty_lines = True
-
+    return gen.next()
 
 class DocumentParser:
     def __init__(self, text: list[PreLine]):
         self._value = text
         # self.tabination_index = 0
-        self.result_list = []
+        self.result_list: DimensionalPreLine = []
         # A stack trace of our current lists we are working
         self.current_stack = [self.result_list]
         self.iterable = iter(text)
@@ -196,54 +171,12 @@ def parse_document(
     ]
     ```
     """
-    ...
-    # tab_char: str | None = tab_character
-    # returnable: list[PreLine | list] = []
-    # first_line = None
+    parser = DocumentParser(text)
+    while current_line := parser.next():
+        next_line = _get_free_tab(current_line, parser)
+        if not next_line:
+            continue
 
-    # gen = iter(text)
-
-    # for line in gen:
-    #     if line.content.strip() == "":
-    #         continue
-        
-    #     first_line = True if first_line is None else False
-
-    #     appendable, first_line, line = _get_free_tab(line, first_line, gen)
-    #     returnable.extend(appendable)
-    #     # If the lines were cleared out by free tab, break out of iter
-    #     if line is None: 
-    #         break
-
-    #     if line.content.strip() == "":
-    #         continue
-
-    #     appendable, first_line, line, tab_char = _get_new_indents(line, tab_char, first_line, gen)
-    #     returnable.extend(appendable)
-    #     if line is None:
-    #         break
-
-    #     returnable.append(line)
-    # #     tab = has_tab(line.content, tab_char, line.number)
-
-    # #     if tab or isinstance(tab, str):
-    # #         if first_line:
-    # #             raise InvalidTabError(f"Unexpected tab on line {line.number}")
-    # #         if isinstance(tab, str):
-    # #             tab_char = tab
-    # #         if tab_char is None:
-    # #             raise InvalidTabError(
-    # #                 "An error has occurred involving tabs. This error should be impossible."
-    # #             )
-    # #         new_line = line.content.removeprefix(tab_char)
-    # #         new_convertible.append(PreLine(new_line, line.number, line.file_index))
-    # #         continue
-
-    # #     if new_convertible:
-    # #         returnable.append(parse_document(new_convertible, tab_char))
-    # #         new_convertible = []
-    # #     returnable.append(line)
-
-    # # if new_convertible:
-    # #     returnable.append(parse_document(new_convertible, tab_char))
-    # return returnable
+        parser.add_line(next_line)
+    
+    return parser.result_list
--- tests/compile_tests/test_precompile/test_indents_to_list.py
+++ tests/compile_tests/test_precompile/test_indents_to_list.py
@@ -1,88 +1,88 @@
-import pytest
+# import pytest
 
-from ducklingscript import PreLine, InvalidTabError
-from ducklingscript.compiler.tab_parse import discover_tab_char, has_tab, parse_document
+# from ducklingscript import PreLine, InvalidTabError
+# from ducklingscript.compiler.tab_parse import discover_tab_char, has_tab, parse_document
 
 
-def test_discover_tab_char():
-    """
-    Test the discover_tab_char function.
-    """
-    assert discover_tab_char("    TEXT") == "    "
-    assert discover_tab_char("\t\tTEXT") == "\t\t"
-    assert discover_tab_char("  \tTEXT") == "  \t"
-    assert discover_tab_char("  TEXT") == "  "
-    assert discover_tab_char("\tTEXT") == "\t"
+# def test_discover_tab_char():
+#     """
+#     Test the discover_tab_char function.
+#     """
+#     assert discover_tab_char("    TEXT") == "    "
+#     assert discover_tab_char("\t\tTEXT") == "\t\t"
+#     assert discover_tab_char("  \tTEXT") == "  \t"
+#     assert discover_tab_char("  TEXT") == "  "
+#     assert discover_tab_char("\tTEXT") == "\t"
 
-def test_discover_tab_char_no_tabs():
-    """
-    Test the discover_tab_char function with no tabs.
-    """
-    assert discover_tab_char("TEXT") == ""
+# def test_discover_tab_char_no_tabs():
+#     """
+#     Test the discover_tab_char function with no tabs.
+#     """
+#     assert discover_tab_char("TEXT") == ""
 
-def test_discover_tab_char_no_text():
-    """
-    Test the discover_tab_char function with no text.
-    """
-    assert discover_tab_char("    ") == ""
-    assert discover_tab_char("\t\t") == ""
-    assert discover_tab_char("  \t") == ""
-    assert discover_tab_char("  ") == ""
-    assert discover_tab_char("\t") == ""
+# def test_discover_tab_char_no_text():
+#     """
+#     Test the discover_tab_char function with no text.
+#     """
+#     assert discover_tab_char("    ") == ""
+#     assert discover_tab_char("\t\t") == ""
+#     assert discover_tab_char("  \t") == ""
+#     assert discover_tab_char("  ") == ""
+#     assert discover_tab_char("\t") == ""
 
-def test_has_tab():
-    """
-    Test the has_tab function.
-    """
-    assert has_tab("    TEXT", "    ", 1) is True
-    assert has_tab("\t\tTEXT", "\t\t", 1) is True
-    assert has_tab("  \tTEXT", "  \t", 1) is True
-    assert has_tab("  TEXT", "  ", 1) is True
-    assert has_tab("\tTEXT", "\t", 1) is True
-    assert has_tab("TEXT", None, 1) is False
+# def test_has_tab():
+#     """
+#     Test the has_tab function.
+#     """
+#     assert has_tab("    TEXT", "    ", 1) is True
+#     assert has_tab("\t\tTEXT", "\t\t", 1) is True
+#     assert has_tab("  \tTEXT", "  \t", 1) is True
+#     assert has_tab("  TEXT", "  ", 1) is True
+#     assert has_tab("\tTEXT", "\t", 1) is True
+#     assert has_tab("TEXT", None, 1) is False
 
-def test_has_tab_invalid():
-    """
-    Test the has_tab function with invalid inputs.
-    """
-    with pytest.raises(InvalidTabError):
-        has_tab("  TEXT", "    ", 1)
+# def test_has_tab_invalid():
+#     """
+#     Test the has_tab function with invalid inputs.
+#     """
+#     with pytest.raises(InvalidTabError):
+#         has_tab("  TEXT", "    ", 1)
     
-    with pytest.raises(InvalidTabError):
-        has_tab("\tTEXT", "\t\t", 1)
+#     with pytest.raises(InvalidTabError):
+#         has_tab("\tTEXT", "\t\t", 1)
     
-    with pytest.raises(InvalidTabError):
-        has_tab("\tTEXT", "  ", 1)
+#     with pytest.raises(InvalidTabError):
+#         has_tab("\tTEXT", "  ", 1)
 
-def test_has_tab_no_tabs():
-    """
-    Test the has_tab function with no tabs.
-    """
-    assert has_tab("TEXT", None, 1) is False
-    assert has_tab("    TEXT", None, 1) == "    "
-    assert has_tab("\t\tTEXT", None, 1) == "\t\t"
-    assert has_tab("  \tTEXT", None, 1) == "  \t"
-    assert has_tab("  TEXT", None, 1) == "  "
-    assert has_tab("\tTEXT", None, 1) == "\t"
+# def test_has_tab_no_tabs():
+#     """
+#     Test the has_tab function with no tabs.
+#     """
+#     assert has_tab("TEXT", None, 1) is False
+#     assert has_tab("    TEXT", None, 1) == "    "
+#     assert has_tab("\t\tTEXT", None, 1) == "\t\t"
+#     assert has_tab("  \tTEXT", None, 1) == "  \t"
+#     assert has_tab("  TEXT", None, 1) == "  "
+#     assert has_tab("\tTEXT", None, 1) == "\t"
     
-def test_parse_document():
-    """
-    Test the parse_document function.
-    """
-    text = [
-        "line1",
-        "    line2",
-        "        line3",
-        "    line4"
-    ]
-    expected_output = [
-        "line1",
-        [
-            "line2",
-            [
-                "line3"
-            ],
-            "line4"
-        ]
-    ]
-    assert parse_document(PreLine.convert_to(text, 0)) == expected_output
+# def test_parse_document():
+#     """
+#     Test the parse_document function.
+#     """
+#     text = [
+#         "line1",
+#         "    line2",
+#         "        line3",
+#         "    line4"
+#     ]
+#     expected_output = [
+#         "line1",
+#         [
+#             "line2",
+#             [
+#                 "line3"
+#             ],
+#             "line4"
+#         ]
+#     ]
+#     assert parse_document(PreLine.convert_to(text, 0)) == expected_output
